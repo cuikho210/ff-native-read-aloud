c02bc2a3c8c28f72913065cb35d66f82e48dc024
cuikho210 <tonykms210@gmail.com>
2025-03-19 01:36:29 +0700

    refactor(src/store.ts): use generic storeOption and loadOption

--- src/store.ts
+++ src/store.ts
@@ -2,45 +2,56 @@ export const keyPitch = "pitch";
 export const keyVolume = "volume";
 export const keyPlaybackRate = "playback-rate";
 export const keyVoice = "voice-index";
+export const keySpeakerId = "speaker-id";
+export const keyDriver = "driver";
 
 export async function storeVoiceIndex(value: number) {
-  await storeNumberOption(keyVoice, value);
+  await storeOption(keyVoice, value);
 }
-
 export async function loadVoiceIndex(): Promise<number | undefined> {
-  return await loadNumberOption(keyVoice);
+  return await loadOption(keyVoice);
 }
 
 export async function storePitch(value: number) {
-  await storeNumberOption(keyPitch, value);
+  await storeOption(keyPitch, value);
+}
+export async function loadPitch(): Promise<number> {
+  return (await loadOption(keyPitch)) ?? 1;
 }
 
 export async function storeVolume(value: number) {
-  await storeNumberOption(keyVolume, value);
+  await storeOption(keyVolume, value);
+}
+export async function loadVolume(): Promise<number> {
+  return (await loadOption(keyVolume)) ?? 1;
+}
+
+export async function storeSpeakerId(value: number) {
+  return storeOption(keySpeakerId, value);
+}
+export async function loadSpeakerId(): Promise<number> {
+  return (await loadOption(keySpeakerId)) ?? 1;
 }
 
 export async function storePlaybackRate(value: number) {
-  await storeNumberOption(keyPlaybackRate, value);
+  await storeOption(keyPlaybackRate, value);
 }
-
-export async function loadPitch(): Promise<number> {
-  return (await loadNumberOption(keyPitch)) ?? 1;
-}
-
-export async function loadVolume(): Promise<number> {
-  return (await loadNumberOption(keyVolume)) ?? 1;
-}
-
 export async function loadPlaybackRate(): Promise<number> {
-  return (await loadNumberOption(keyPlaybackRate)) ?? 1;
+  return (await loadOption(keyPlaybackRate)) ?? 1;
 }
 
-async function storeNumberOption(key: string, value: number) {
-  const data: Record<string, number> = {};
+export async function storeDriver(value: string) {
+  await storeOption(keyDriver, value);
+}
+export async function loadDriver(): Promise<string | undefined> {
+  return await loadOption(keyDriver);
+}
+
+async function storeOption<T>(key: string, value: T) {
+  const data: Record<string, T> = {};
   data[key] = value;
   await browser.storage.local.set(data);
 }
-
-async function loadNumberOption(key: string): Promise<number | undefined> {
+async function loadOption<T>(key: string): Promise<T | undefined> {
   return (await browser.storage.local.get(key))[key];
 }
